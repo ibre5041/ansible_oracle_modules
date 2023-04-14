dd7ce774be94f1e02ea69742a59d44c086b623b1
Ivan Brezina <ivan.brezina@merck.com>
2023-04-14 16:11:38 +0200

    oracle_directory reworked

--- plugins/modules/oracle_directory.py
+++ plugins/modules/oracle_directory.py
@@ -53,154 +53,110 @@ options:
 notes:
     - cx_Oracle needs to be installed
 requirements: [ "cx_Oracle" ]
-author: Mikael Sandström, oravirt@gmail.com, @oravirt
+author: 
+    - Mikael Sandström, oravirt@gmail.com, @oravirt
+    - Ivan Brezina
 '''
 
 EXAMPLES = '''
 
+    - name: create a directory
+      oracle_directory:
+        mode: sysdba
+        directory_name: TEST_DIRECTORY
+        directory_path: /oracle/directory
 
 '''
 
-try:
-    import cx_Oracle
-except ImportError:
-    cx_oracle_exists = False
-else:
-    cx_oracle_exists = True
 
 # Check if the directory exists
-def check_directory_exists(module, msg, cursor, directory_name):
-
-    if not(directory_name):
-        module.fail_json(msg='Error: Missing directory name', changed=False)
-        return False
-
-    sql = 'select count(*) from dba_directories where directory_name= upper(\'%s\')' % directory_name
+def check_directory_exists(conn, directory_name):
+    sql = "select directory_name, directory_path from dba_directories where directory_name = upper(:directory_name)"
+    r = conn.execute_select_to_dict(sql, {"directory_name": directory_name}, fetchone=True)
+    return set(r.items())
 
 
-    try:
-            cursor.execute(sql)
-            result = cursor.fetchone()[0]
-    except cx_Oracle.DatabaseError as exc:
-            error, = exc.args
-            msg = error.message+ 'sql: ' + sql
-            return False
+def ensure_directory(conn, module, current_directory):
+    directory_path = module.params["directory_path"]
+    directory_name = module.params["directory_name"]
 
-    if result > 0:
-        return True
-    else:
-        return False
-
-def ensure_directory(module, msg, cursor, directory_name, directory_path, directory_mode):
-
-    if check_directory_exists(module, msg, cursor, directory_name):
-        check_path_sql = 'select directory_path from dba_directories where directory_name = upper(\'%s\')' % directory_name
-        _curr_path = execute_sql_get(module, msg, cursor, check_path_sql)
-
-        if _curr_path[0][0] != directory_path:
-            if directory_mode == 'enforce':
-                directory_sql = 'create or replace directory %s as \'%s\'' % (directory_name, directory_path)
-                if execute_sql(module,msg,cursor,directory_sql):
-                    module.exit_json(msg='Directory %s, changed to path: %s (old path: %s)' % (directory_name, directory_path,_curr_path[0][0]), changed=True)
+    if current_directory:
+        current_path = next(v for (a, v) in current_directory if a == 'directory_path')
+        if current_path == directory_path:
+            msg = 'Directory %s already exists (%s)' % (directory_name, directory_path)
+            module.exit_json(msg, changed=False)
         else:
-            module.exit_json(msg='Directory %s already exists (%s)' % (directory_name, directory_path), changed=False)
+            directory_sql = 'create or replace directory %s as \'%s\'' % (directory_name, directory_path)
+            conn.execute_ddl(directory_sql)
+            msg = 'Directory %s, changed to path: %s (old path: %s)'% (directory_name, directory_path, current_path)
+            module.exit_json(msg, changed=conn.changed, ddls=conn.ddls)
     else:
-        directory_sql = 'create directory %s as \'%s\'' % (directory_name, directory_path)
-
-        if execute_sql(module,msg,cursor,directory_sql):
-            msg = 'Directory: %s, created with path: %s' % (directory_name, directory_path)
-            module.exit_json(msg=msg, changed=True)
-
-def drop_directory(module, msg, cursor, directory_name):
-    drop_sql = 'drop directory %s' % (directory_name)
-
-    if execute_sql(module,msg,cursor,drop_sql):
-        module.exit_json(msg='Directory %s successfully dropped' % (directory_name), changed=True)
+        directory_sql = "create directory %s as '%s'" % (directory_name, directory_path)
+        conn.execute_ddl(directory_sql)
+        msg = 'Directory: %s, created with path: %s' % (directory_name, directory_path)
+        module.exit_json(msg=msg, changed=conn.changed, ddls=conn.ddls)
 
 
-def execute_sql(module, msg, cursor, sql):
+def drop_directory(conn, module):
+    directory_name = module.params["directory_name"]
+    drop_sql = 'drop directory %s' % directory_name
 
-    try:
-        cursor.execute(sql)
-    except cx_Oracle.DatabaseError as exc:
-        error, = exc.args
-        msg = 'Something went wrong while executing sql - %s sql: %s' % (error.message, sql)
-        module.fail_json(msg=msg, changed=False)
-        return False
-    return True
-
-
-def execute_sql_get(module, msg, cursor, sql):
-
-    try:
-        cursor.execute(sql)
-        result = (cursor.fetchall())
-    except cx_Oracle.DatabaseError as exc:
-        error, = exc.args
-        msg = 'Something went wrong while executing sql_get - %s sql: %s' % (error.message, sql)
-        module.fail_json(msg=msg, changed=False)
-        return False
-    return result
-
+    conn.execute_ddl(drop_sql)
+    msg = 'Directory %s successfully dropped' % directory_name
+    module.exit_json(msg=msg, changed=conn.changed, ddls=conn.ddls)
 
 
 def main():
-
-    msg = ['']
-    global state
     module = AnsibleModule(
         argument_spec = dict(
-            hostname      = dict(default='localhost'),
-            port          = dict(default=1521, type='int'),
-            service_name  = dict(required=False),
-            user          = dict(required=False),
-            password      = dict(required=False, no_log=True),
-            mode          = dict(default='normal', choices=["normal","sysdba"]),
+            user          = dict(required=False, aliases=['un', 'username']),
+            password      = dict(required=False, no_log=True, aliases=['pw']),
+            mode          = dict(default='normal', choices=["normal", "sysdba"]),
+            hostname      = dict(required=False, default='localhost', aliases=['host']),
+            port          = dict(required=False, default=1521, type='int'),
+            service_name  = dict(required=False, aliases=['sn']),
+            oracle_home   = dict(required=False, aliases=['oh']),
             directory_name = dict(default=None),
             directory_path = dict(default=None),
             directory_mode = dict(default="enforce", choices=["normal", "enforce"]),
             state          = dict(default="present", choices=["present", "absent"])
-
-        )
+        ),
+        required_together=[['user', 'password']],
+        required_if=[('state', 'present', ('directory_name', 'directory_path'))],
+        supports_check_mode=True
     )
 
     directory_name = module.params["directory_name"]
-    directory_path = module.params["directory_path"]
-    directory_mode = module.params["directory_mode"]
     state = module.params["state"]
 
-    if not cx_oracle_exists:
-        module.fail_json(msg="The cx_Oracle module is required. 'pip install cx_Oracle' should do the trick. If cx_Oracle is installed, make sure ORACLE_HOME & LD_LIBRARY_PATH is set")
+    oc = oracleConnection(module)
 
-    conn = oracle_connect(module)
-    cursor = conn.cursor()
-
+    current_directory = check_directory_exists(oc, directory_name)
     if state == 'present':
-        ensure_directory(module, msg, cursor, directory_name, directory_path, directory_mode)
+        ensure_directory(oc, module, current_directory)
     elif state == 'absent':
-        if check_directory_exists(module, msg, cursor, directory_name):
-            drop_directory(module, msg, cursor, directory_name)
+        if current_directory:
+            drop_directory(oc, module)
         else:
-            msg = 'Directory %s doesn\'t exist' % (directory_name)
+            msg = "Directory %s doesn't exist" % directory_name
             module.exit_json(msg=msg, changed=False)
 
-    else:
-        module.fail_json(msg='Unhandled exit', changed=False)
+    module.fail_json(msg='Unhandled exit', changed=False)
 
 
 from ansible.module_utils.basic import *
 
-# In thise we do import from local project project sub-directory <project-dir>/module_utils
+# In these we do import from local project sub-directory <project-dir>/module_utils
 # While this file is placed in <project-dir>/library
-# No colletions are used
+# No collections are used
 try:
-    from ansible.module_utils.oracle_utils import oracle_connect
+    from ansible.module_utils.oracle_utils import oracleConnection
 except:
     pass
 
-# In thise we do import from collections
+# In these we do import from collections
 try:
-    from ansible_collections.ibre5041.ansible_oracle_modules.plugins.module_utils.oracle_utils import oracle_connect
+    from ansible_collections.ibre5041.ansible_oracle_modules.plugins.module_utils.oracle_utils import oracleConnection
 except:
     pass
 
