4d3999ba97b83dced6891c6caebf909ffae55d3f
Ivan Brezina <ivan.brezina@merck.com>
2023-11-22 18:20:20 +0100

    oracle_parameter.py - reworked, fixed

--- plugins/modules/oracle_parameter.py
+++ plugins/modules/oracle_parameter.py
@@ -71,92 +71,111 @@ oracle_parameter: hostname=remote-db-server service_name=orcl user=system passwo
 # Reset the value of open_cursors
 oracle_parameter: hostname=remote-db-server service_name=orcl user=system password=manager name=db_recovery_file_dest_size state=reset scope=spfile
 
-
 '''
+
+from collections import namedtuple
 
 
 # Check if the parameter exists
 def check_parameter_exists(conn, parameter_name):
     mode = conn.module.params['mode']
-    scope = conn.module.params['scope']
 
     # Check if parameter name is valid
-    if parameter_name.startswith('_') and mode != 'sysdba':
-        msg = 'You need sysdba privs to verify underscore parameters (%s), mode: (%s)' % (parameter_name, mode)
-        conn.module.fail_json(msg=msg, changed=False)
-    elif parameter_name.startswith('_') and mode == 'sysdba':
-        sql = "select lower(ksppinm) from sys.x$ksppi where ksppinm = lower(:parameter_name)"
+    if parameter_name.startswith('_'):
+        sql = """
+        select lower(p.ksppinm) as name, 
+        b.ksppstvl as CURRENT_VALUE, 
+        b.ksppstdfl as DEFAULT_VALUE, 
+        b.ksppstdf as ISDEFAULT, 
+        b.ksppstdvl as DISPLAY_VALUE
+        from x$ksppi p
+        , x$ksppsv b -- X$KSPPSV (Parameter values at system level)
+        where p.indx = b.indx
+        and p.ksppinm = lower(:parameter_name)"""
+        p = conn.execute_select_to_dict (sql, {"parameter_name": parameter_name}, fetchone=True)
+        #{'name': 'compatible', 'current_value': '19.0.0', 'default_value': None, 'isdefault': 'FALSE',
+        # 'display_value': '19.0.0'}
     else:
-        sql = "select lower(name) from v$parameter where name = lower(:parameter_name)"
-    n = conn.execute_select(sql, {"parameter_name": parameter_name}, fetchone=True)
-    if not n[0]:
-        conn.module.fail_json(msg="Could not verify parameter name: {}".format(parameter_name), changed=False)
+        sql = """
+        select lower(name) as name, 
+        value as CURRENT_VALUE, 
+        ISMODIFIED,-- was modified since startup
+        ISDEFAULT,
+        DISPLAY_VALUE  
+        from v$parameter where name = lower(:parameter_name)
+        """
+        p = conn.execute_select_to_dict(sql, {"parameter_name": parameter_name}, fetchone=True)
 
-    if scope == 'spfile':
-        parameter_source = 'v$spparameter'
-    else:
-        parameter_source = 'v$parameter'
+    if not p:
+        conn.module.fail_json(msg="Invalid parameter: {}".format(parameter_name), changed=False)
 
     if mode == 'sysdba':
-        if scope == 'spfile':
-            sql = "select lower(KSPSPFFTCTXSPDVALUE) from x$kspspfile where lower(KSPSPFFTCTXSPNAME) = lower(:parameter_name)"
-        else:
-            sql = "select lower(y.ksppstdvl) from sys.x$ksppi x, sys.x$ksppcv y where x.indx = y.indx and x.ksppinm = lower(:parameter_name)"
+        sql = """
+            select lower(KSPSPFFTCTXSPNAME) as name, KSPSPFFTCTXSPDVALUE as SPFILE_VALUE 
+            from x$kspspfile where lower(KSPSPFFTCTXSPNAME) = lower(:parameter_name)
+            """
+        s = conn.execute_select_to_dict(sql, {"parameter_name": parameter_name}, fetchone=True)
     else:
-        sql = "select lower(display_value) from %s where name = lower(:parameter_name)" % parameter_source
-    v = conn.execute_select(sql, {"parameter_name": parameter_name}, fetchone=True)
+        sql = "select lower(name) as name, SPFILE_VALUE from v$spparameter where name = lower(:parameter_name)"
+        s = conn.execute_select_to_dict(sql, {"parameter_name": parameter_name}, fetchone=True)
 
-    return v[0]
+    p.update(s)
+    return namedtuple("Parameter", p.keys())(*p.values())
 
 
-def modify_parameter(conn, module, current_parameter):
+def modify_parameter(conn, module, parameter):
     parameter_name = module.params['parameter_name']
     value = module.params['value']
     comment = module.params['comment']
     scope = module.params['scope']
     sid = module.params['sid']
 
-    if current_parameter == value.lower() or not current_parameter and value == "''":
-        module.exit_json(msg='The parameter (%s) is already set to %s' % (parameter_name, value), changed=False)
-        return True
+    if parameter_name.startswith("_"):
+        parameter_name = '"{}"'.format(parameter_name)
 
-    sql = 'alter system set %s=%s ' % (parameter_name, value)
+    sql = 'alter system set {}="{}" '.format(parameter_name, value)
     if comment:
         sql += " comment='%s'" % comment
-
     sql += " scope=%s sid='%s'" % (scope, sid)
+    if sql in conn.ddls:
+        return
     conn.execute_ddl(sql)
-    msg = 'The parameter (%s) has been changed successfully, new: %s, old: %s' % (parameter_name, value, current_parameter)
+    msg = 'The parameter ({}) has been changed successfully, new: "{}", old: {}'.format(parameter_name, value, str(parameter))
     module.exit_json(msg=msg, changed=conn.changed, ddls=conn.ddls)
 
 
-def reset_parameter(conn, module):
+def reset_parameter(conn, module, parameter):
     parameter_name = module.params['parameter_name']
     scope = module.params['scope']
     sid = module.params['sid']
 
-    sql = "alter system reset %s scope=%s sid='%s'" % (parameter_name, scope, sid)
+    if parameter_name.startswith("_"):
+        parameter_name = '"{}"'.format(parameter_name)
+
+    sql = "alter system reset {} scope={} sid='{}'".format(parameter_name, scope, sid)
+    if sql in conn.ddls:
+        return
     conn.execute_ddl(sql)
-    msg = 'The parameter (%s) has been reset to its default value' % parameter_name
+    msg = 'The parameter ({}) has been reset to its default value'.format(parameter_name)
     module.exit_json(msg=msg, changed=conn.changed, ddls=conn.ddls)
 
 
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            user          = dict(required=False, aliases=['un', 'username']),
-            password      = dict(required=False, no_log=True, aliases=['pw']),
-            mode          = dict(default='normal', choices=["normal", "sysdba"]),
-            hostname      = dict(required=False, default='localhost', aliases=['host']),
-            port          = dict(required=False, default=1521, type='int'),
-            service_name  = dict(required=False, aliases=['sn']),
-            oracle_home   = dict(required=False, aliases=['oh']),
-            parameter_name = dict(default=None, aliases=['parameter', 'name']),
-            value         = dict(default=None),
-            comment       = dict(default=None),
-            state         = dict(default="present", choices=["present", "absent", "reset"]),
-            scope         = dict(default="both", choices=["both", "spfile", "memory"]),
-            sid           = dict(default="*"),
+        argument_spec=dict(
+            user=dict(required=False, aliases=['un', 'username']),
+            password=dict(required=False, no_log=True, aliases=['pw']),
+            mode=dict(default='normal', choices=["normal", "sysdba"]),
+            hostname=dict(required=False, default='localhost', aliases=['host']),
+            port=dict(required=False, default=1521, type='int'),
+            service_name=dict(required=False, aliases=['sn']),
+            oracle_home=dict(required=False, aliases=['oh']),
+            parameter_name=dict(default=None, aliases=['parameter', 'name']),
+            value=dict(default=None),
+            comment=dict(default=None),
+            state=dict(default="present", choices=["present", "absent", "reset"]),
+            scope=dict(default="both", choices=["both", "spfile", "memory"]),
+            sid=dict(default="*"),
         ),
         required_if=[('mode', 'normal', ('username', 'password', 'service_name')),
                      ('state', 'present', ['value'])],
@@ -165,19 +184,35 @@ def main():
     )
 
     parameter_name = module.params["parameter_name"]
+    value = module.params["value"]
+    mode = module.params["mode"]
     state = module.params["state"]
+    scope = module.params["scope"]
 
-    oc = oracleConnection(module)
+    if parameter_name.startswith('_') and mode != 'sysdba':
+        msg = 'You need sysdba privs to verify underscore parameters (%s), mode: (%s)' % (parameter_name, mode)
+        module.fail_json(msg=msg, changed=False)
 
-    parameter = check_parameter_exists(oc, parameter_name)
-    if state == 'present':
-        if parameter:
-            modify_parameter(oc, module)
-    elif state == 'reset' or state == 'absent':
-        if parameter:
-            reset_parameter(oc, module)
+    conn = oracleConnection(module)
+
+    parameter = check_parameter_exists(conn, parameter_name)
+    if state in ['reset', 'absent']:
+        if parameter.spfile_value and scope in ['spfile', 'both']:
+            m = reset_parameter(conn, module, parameter)
+            msg.append(m)
+        elif parameter.display_value:
+            m = reset_parameter(conn, module, parameter)
+            msg.append(m)
         else:
-            module.fail_json(msg="Parameter %s doesn't exist or sysdba privileges needed.", changed=False)
+            module.exit_json(msg="Nothing to do for: {}".format(str(parameter)), changed=False)
+    elif state == 'present':
+        if scope in ['spfile', 'both'] and parameter.spfile_value != value:
+            modify_parameter(conn, module, parameter)
+        elif scope in ['memory', 'both'] and parameter.display_value != value:
+            modify_parameter(conn, module, parameter)
+        else:
+            module.exit_json(msg="Nothing to do for: {}".format(str(parameter)), changed=False)
+
     module.fail_json(msg='Unhandled exit', changed=False)
 
 
@@ -186,9 +221,9 @@ from ansible.module_utils.basic import *
 # In this case we do import from local project sub-directory <project-dir>/module_utils
 # While this file is placed in <project-dir>/library
 # No collections are used
-#try:
+# try:
 #    from ansible.module_utils.oracle_utils import oracleConnection
-#except:
+# except:
 #    pass
 
 # In this case we do import from collections
@@ -196,7 +231,6 @@ try:
     from ansible_collections.ibre5041.ansible_oracle_modules.plugins.module_utils.oracle_utils import oracleConnection
 except:
     pass
-
 
 if __name__ == '__main__':
     main()
