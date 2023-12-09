cf790817c241b7a9d6eb64100a1d9acd9275db5f
Ivan Brezina <ibrezina@ibrezina.net>
2023-12-09 18:22:06 +0100

    doc update

--- plugins/module_utils/describe_patchset.py
+++ plugins/module_utils/describe_patchset.py
@@ -1,5 +1,52 @@
 #!/usr/bin/env python3
 
+DOCUMENTATION = '''
+---
+module: describe_patchset
+short_description: Parse .xml metadata in oracle patch directory
+description:
+  - This is not ansible module, this is standalone commandline tool
+  - See EXAMPLES
+author:
+  - Ivan Brezina
+'''
+
+EXAMPLES = '''
+describe_patchset.py -h
+usage: describe_patchset.py [-h] -d DIRECTORY
+
+Describe patches in Oracle PatchSet
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DIRECTORY, --directory DIRECTORY
+
+./describe_patchset.py -d /install/
+PATCHSET:/install/35742441:COMBO OF OJVM RU COMPONENT 19.21.0.0.231017 + GI RU 19.21.0.0.231017
+PATCHSET:/install/35742441/35648110:OJVM RELEASE UPDATE 19.21.0.0.0
+BUNDLEPART:/install/35742441/35642822/35655527:(cluster,rac_database,oracle_database,has)
+BUNDLEPART:/install/35742441/35642822/35652062:(cluster,has)
+BUNDLEPART:/install/35742441/35642822/33575402:(cluster,has)
+BUNDLEPART:/install/35742441/35642822/35553096:(cluster,has)
+BUNDLEPART:/install/35742441/35642822/35643107:(cluster,rac_database,oracle_database,has)
+ONE-OFF:/install/35638318:JDK BUNDLE PATCH 19.0.0.0.231017:()
+ONE-OFF:/install/35742441/35642822/35655527:OCW RELEASE UPDATE 19.21.0.0.0 (35655527):(cluster,rac_database,oracle_database,has)
+ONE-OFF:/install/35742441/35642822/35643107:Database Release Update : 19.21.0.0.231017 (35643107):(cluster,rac_database,oracle_database,has)
+ONE-OFF:/install/35742441/35642822/35652062:ACFS RELEASE UPDATE 19.21.0.0.0 (35652062):(cluster,has)
+ONE-OFF:/install/35742441/35642822/35553096:TOMCAT RELEASE UPDATE 19.0.0.0.0 (35553096):(cluster,has)
+ONE-OFF:/install/35742441/35642822/33575402:DBWLM RELEASE UPDATE 19.0.0.0.0 (33575402):(cluster,has)
+ONE-OFF:/install/35742441/35648110:OJVM RELEASE UPDATE: 19.21.0.0.231017 (35648110):()
+
+./describe_patchset.py -d $ORACLE_HOME/inventory
+ONE-OFF:/oracle/product/19.21.0.0/db1/inventory/oneoffs/35655527:OCW RELEASE UPDATE 19.21.0.0.0 (35655527):(cluster,rac_database,oracle_database,has)
+ONE-OFF:/oracle/product/19.21.0.0/db1/inventory/oneoffs/35648110:OJVM RELEASE UPDATE: 19.21.0.0.231017 (35648110):()
+ONE-OFF:/oracle/product/19.21.0.0/db1/inventory/oneoffs/35643107:Database Release Update : 19.21.0.0.231017 (35643107):(cluster,rac_database,oracle_database,has)
+ONE-OFF:/oracle/product/19.21.0.0/db1/inventory/oneoffs/29585399:OCW RELEASE UPDATE 19.3.0.0.0 (29585399):(cluster,rac_database,oracle_database,has)
+ONE-OFF:/oracle/product/19.21.0.0/db1/inventory/oneoffs/29517242:Database Release Update : 19.3.0.0.190416 (29517242):(cluster,rac_database,oracle_database,has)
+ONE-OFF:/oracle/product/19.21.0.0/db1/inventory/oneoffs/35638318:JDK BUNDLE PATCH 19.0.0.0.231017:()
+
+'''
+
 import os
 import argparse
 import glob
